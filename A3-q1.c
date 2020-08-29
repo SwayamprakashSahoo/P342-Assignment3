098170ef952d14d105f527a334c957bb55e78432
SwayamprakashSahoo <42139077+SwayamprakashSahoo@users.noreply.github.com>
2020-08-29 12:20:23 +0530

    Add files via upload

---
+++ A3-q1.c
@@ -0,0 +1,58 @@
+//Program for Gauss-Jordan problem analysis
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	printf("Please enter the number of variables in the equations: ");
+	int n;
+	scanf("%d", &n);
+	float mat[20][20];
+	FILE* temp1 = fopen("Set1.txt", "r");
+	if (temp1 == NULL)
+		exit(1);
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n + 1; j++)
+		{
+			fscanf(temp1, "%f ", &mat[i][j]);
+		}
+	}
+
+	printf("Matrix representation for given set of equations is:\n ");
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			printf(" %d",(int) (mat[i][j]));
+		}
+		printf("   x%d", i + 1);
+		printf(" = %f", mat[i][n]);
+		printf("\n ");
+	}
+	float c, x[20];
+	for (int j = 0; j < n; j++)
+	{
+		for ( int i = 0; i < n; i++)
+		{
+			if (i != j)
+			{
+				c = mat[i][j] / mat[j][j];
+				for (int k = 0; k < n + 1; k++)
+				{
+					mat[i][k] = mat[i][k] - c * mat[j][k];
+				}
+			}
+		}
+	}
+	printf("\nThe solution is:\n");
+	for (int i = 0; i < n; i++)
+	{
+		x[i] = mat[i][n] / mat[i][i];
+		printf("\n x%d=%f\n", i+1, x[i]);
+	}
+	return(0);
+}
