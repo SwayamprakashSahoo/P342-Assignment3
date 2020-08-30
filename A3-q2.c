ccd8a10baee534ea9272634f426c47fc22fa1513
SwayamprakashSahoo <42139077+SwayamprakashSahoo@users.noreply.github.com>
2020-08-30 19:14:10 +0530

    Update A3-q2.c

--- A3-q2.c
+++ A3-q2.c
@@ -92,3 +92,24 @@ void GaussJordan(float mat[][20], int var)
 		printf("\n x%d=%f\n", i + 1, x[i]);
 	}
 }
+
+
+
+
+/*
+*******************OUTPUT***********************
+Please enter the number of variables in the equations: 3
+Matrix representation for given set of equations is:
+  0 2 5   x1 = 1.000000
+  3 -1 2   x2 = -2.000000
+  1 -1 3   x3 = 3.000000
+
+The solution is:
+
+ x1=-2.000000
+
+ x2=-2.000000
+
+ x3=1.000000
+ ***********************************************
+ */
