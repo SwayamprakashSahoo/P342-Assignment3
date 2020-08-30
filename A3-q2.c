d2d5162d65fd0c5729c512b619ef23d9c870a98b
SwayamprakashSahoo <42139077+SwayamprakashSahoo@users.noreply.github.com>
2020-08-30 19:11:38 +0530

    Add files via upload

---
+++ A3-q2.c
@@ -0,0 +1,94 @@
+//Program for Gauss Jordan analysis with partial pivoting
+#include <stdio.h>
+#include <stdlib.h>
+
+void pivot(float mat[][20], int var);
+void GaussJordan(float mat[][20], int var);
+
+int main()
+{
+	printf("Please enter the number of variables in the equations: ");
+	int var;
+	scanf("%d", &var);
+	float mat[20][20];
+	FILE* temp1 = fopen("Set2.txt", "r");
+	if (temp1 == NULL)
+		exit(1);
+
+	for (int i = 0; i < var; i++)
+	{
+		for (int j = 0; j < var + 1; j++)
+		{
+			fscanf(temp1, "%f ", &mat[i][j]);
+		}
+	}
+
+	printf("Matrix representation for given set of equations is:\n ");
+	for (int i = 0; i < var; i++)
+	{
+		for (int j = 0; j < var; j++)
+		{
+			printf(" %d", (int)(mat[i][j]));
+		}
+		printf("   x%d", i + 1);
+		printf(" = %f", mat[i][var]);
+		printf("\n ");
+	}
+
+	GaussJordan(mat, var);
+	return(0);
+}
+
+void pivot(float mat[][20], int var)
+{
+	for (int i = 0; i < var - 1; i++)
+	{
+		if (mat[i][i] == 0)
+		{
+			for (int j = i + 1; j < var; j++)
+			{
+				if (abs(mat[j][i]) > mat[i][i])
+				{
+					for (int k = i; k < var + 1; k++)
+					{
+						float temp = mat[i][k];
+						mat[i][k] = mat[j][k];
+						mat[j][k] = temp;
+						continue;
+					}
+				}
+			}
+		}
+	}
+}
+
+void GaussJordan(float mat[][20], int var)
+{
+	for (int i = 0; i < var; i++)
+	{
+		pivot(mat, var);
+	}
+
+	float c, x[20];
+	for (int j = 0; j < var; j++)
+	{
+		for (int i = 0; i < var; i++)
+		{
+			if (i != j)
+			{
+				c = mat[i][j] / mat[j][j];
+				for (int k = 0; k < var + 1; k++)
+				{
+					mat[i][k] = mat[i][k] - c * mat[j][k];
+				}
+			}
+		}
+	}
+
+	printf("\nThe solution is:\n");
+	for (int i = 0; i < var; i++)
+	{
+		x[i] = mat[i][var] / mat[i][i];
+		printf("\n x%d=%f\n", i + 1, x[i]);
+	}
+}
